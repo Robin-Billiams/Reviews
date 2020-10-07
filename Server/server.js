8987c8cfdbe65bd5e21c4ade55c0ce64f8e3cbb0
vargas055 <juanjo220393@gmail.com>
2020-10-07 19:05:55 -0400

    git ignore updated

--- Server/server.js
+++ Server/server.js
@@ -2,5 +2,5 @@ const app = require("./app")
 const port = 3001
 
 app.listen(port, () => {
-    console.log(`Reviews module Server.js server listening on http://localhost:${port}`)
+    console.log(`Reviews module Server.js server listening on http://locallhost:${port}`)
 })
